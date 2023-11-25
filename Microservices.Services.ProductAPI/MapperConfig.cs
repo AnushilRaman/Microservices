43784565745809760cf51b632baaaaeb2b48525a
Anushil Raman <anushilraman1993@gmail.com>
2023-11-26 02:08:10 +0530

    Configuring Models and Mapper for API

---
+++ Microservices.Services.ProductAPI/MapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microservices.Services.ProductAPI.Models;
+using Microservices.Services.ProductAPI.Models.Dto;
+
+namespace Microservices.Services.ProductAPI
+{
+    public class MapperConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
