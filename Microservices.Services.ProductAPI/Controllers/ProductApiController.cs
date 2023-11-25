1e0534f9549ea38430a2bfc47e79e0131691c15d
Anushil Raman <anushilraman1993@gmail.com>
2023-11-26 02:17:56 +0530

    Product API Crud Operation

---
+++ Microservices.Services.ProductAPI/Controllers/ProductApiController.cs
@@ -0,0 +1,130 @@
+﻿using AutoMapper;
+using Azure;
+using Microservices.Services.ProductAPI.Data;
+using Microservices.Services.ProductAPI.Models;
+using Microservices.Services.ProductAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservices.Services.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+        private ResponseDto _response;
+
+        public ProductApiController(AppDbContext appDbContext,IMapper mapper) 
+        {
+            this._db = appDbContext;
+            this._mapper = mapper;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<ResponseDto> Get() 
+        {
+            try
+            {
+                IEnumerable<Product> objList = await _db.Products.ToListAsync();
+                _response.Result = _mapper.Map<IEnumerable<ProductDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ResponseDto> Get(int id)
+        {
+            try
+            {
+                Product obj = await _db.Products.FirstAsync(u => u.ProductId == id);
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("Name/{Name}")]
+        public async Task<ResponseDto> GetByName(string Name)
+        {
+            try
+            {
+                Product obj = await _db.Products.FirstAsync(u => u.Name == Name);
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPost]
+        public async Task<ResponseDto> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(productDto);
+                await _db.Products.AddAsync(obj);
+                await _db.SaveChangesAsync();
+
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPut]
+        public async Task<ResponseDto> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(productDto);
+                _db.Products.Update(obj);
+                await _db.SaveChangesAsync();
+
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ResponseDto> Delete(int id)
+        {
+            try
+            {
+                Product obj = _db.Products.First(u => u.ProductId == id);
+                _db.Products.Remove(obj);
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
