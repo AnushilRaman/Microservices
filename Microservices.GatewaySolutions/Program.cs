a2a60b72d51b74f0e7782657d622d9a5411d07d7
Anushil Raman <anushilraman1993@gmail.com>
2023-12-09 23:06:18 +0530

    Setup for API Gateway using third party package Ocelot

---
+++ Microservices.GatewaySolutions/Program.cs
@@ -0,0 +1,17 @@
+using Microservices.GatewaySolutions.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+builder.AddAppAuthentication();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseOcelot();
+
+app.Run();
