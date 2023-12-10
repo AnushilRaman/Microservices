498157fd65efb191fbd38be3d2d3bf8e6f986200
Anushil Raman <anushilraman1993@gmail.com>
2023-12-11 02:08:41 +0530

    Azure deployment of Gateway

--- Microservices.GatewaySolutions/Program.cs
+++ Microservices.GatewaySolutions/Program.cs
@@ -4,7 +4,14 @@ using Ocelot.Middleware;
 using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+if (builder.Environment.EnvironmentName.ToLower().ToString().Equals("production"))
+    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+
+else
+    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+
 builder.Services.AddOcelot(builder.Configuration);
 builder.AddAppAuthentication();
 
